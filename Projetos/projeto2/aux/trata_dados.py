3c74feb0b8d47fca4c100fe20c546be8a3ff31b6
eru-iluv <emc2rcf@gmail.com>
2023-04-05 12:11:56 -0300

    Altera tratar dados para funcionar :)

---
+++ Projetos/projeto2/aux/trata_dados.py
@@ -0,0 +1,21 @@
+import sys
+from decimal import Decimal
+
+inputHandle = open("tarefa-1/saida-1", 'r')
+outputFile = sys.argv[1]
+outputHandle = open("dados/{}".format(outputFile), 'w')
+spacing = 0.06
+
+lines = inputHandle.readlines()
+size_t = len(lines)
+
+lines = [lines[i] for i in range(0, size_t, 10)]
+dataInput = [[Decimal(value) for value in line.split()] for line in lines] 
+size_x = len(dataInput[1])
+dataOutput = [[str((dataInput[i][j]+i)*10) for i in range(size_t//10)] for j in range(size_x)]
+for dataLine in dataOutput:
+    outputHandle.write("  ".join(dataLine) + "\n")
+
+inputHandle.close()
+outputHandle.close()
+    
